facef44997cbe92b2ac9644d6dd561a515fc6f29
pwater44 <nvrithwik@hotmail.com>
2020-06-01 16:28:03 -0400

    Welcome Service Header

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export class HelloWorldBean {
   constructor(public message:string) {}
@@ -18,7 +18,21 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldBeanServiceWithPathVariable(name) {
+    let basicAuthHeaderString = this.executeHelloWorldBeanService()
+    let header = new HttpHeaders ({
+      Authorization: basicAuthHeaderString})
     return this.http.get<HelloWorldBean>(
-      `http://localhost:8080/hello-world/path-variable/${name}`);
+      `http://localhost:8080/hello-world/path-variable/${name}`, { headers: header});
   }
+
+  createBasicAuthenticationHttpHeader() {
+    let username = 'user'
+    let password = 'simtek'
+    let basicAuthHeaderString = 'Basic ' + window.btoa (
+    username + password)
+    return basicAuthHeaderString
+  }
+
+
+
 }
